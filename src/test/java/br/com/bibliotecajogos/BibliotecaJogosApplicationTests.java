1403cc120ad022bc63a045ebfda3a19824fef587
Sthefany Souza <sthefany.souza1006@gmail.com>
2025-09-30 23:50:35 -0300

    .

---
+++ src/test/java/br/com/bibliotecajogos/BibliotecaJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bibliotecajogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaJogosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
